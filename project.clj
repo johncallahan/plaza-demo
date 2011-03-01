642fa129d0e3257a9a3da445a84d35a8f5d2ba32
johncallahan <jcallahan@acm.org>
2011-03-01 19:00:51 +0000

    Adding lein project skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject plaza-demo "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [plaza "0.0.5-SNAPSHOT" :exclusions [org.clojure/clojure-contrib org.clojure/clojure]]]
+  :main     plaza-demo.webapp)
